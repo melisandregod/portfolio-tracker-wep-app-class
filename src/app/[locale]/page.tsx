3d5148e18f99560e169aea631390eaa75164961c
melisandregod <momomeez01@gmail.com>
2025-10-24 05:34:12 +0700

    fix: redirect-bug

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,18 @@
-// app/page.tsx
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+// app/[locale]/page.tsx
+import { routing } from "@/i18n/routing";
+import { hasLocale } from "next-intl";
+import { notFound, redirect } from "next/navigation";
 
-export default async function LocaleRedirect() {
-  const cookieLocale = (await cookies()).get("NEXT_LOCALE")?.value || "en";
-  redirect(`/${cookieLocale}/dashboard`);
+export default async function LocaleRedirect({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+  // const cookieLocale = (await cookies()).get("NEXT_LOCALE")?.value || "en";
+  redirect(`/${locale}/dashboard`);
 }
